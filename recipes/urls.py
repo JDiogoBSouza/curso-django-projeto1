e859a44a8e178b5c7bee811d8d62391037c04f1e
Diogo Souza <di0g0-dg@hotmail.com>
2023-03-18 15:08:08 -0400

    Using models with QuerySet inside views.

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,4 +8,6 @@ app_name = "recipes"
 urlpatterns = [
     path('', views.home, name="home"),  # Home
     path('recipes/<int:id>/', views.recipe, name="recipe"),  # Recipes
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),  # Category
 ]
