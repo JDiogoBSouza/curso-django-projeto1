70a1a6aef6605b48b2b2ad745aa9656b0bee713e
Diogo Souza <di0g0-dg@hotmail.com>
2023-03-18 11:21:05 -0400

    Adding relations and creating the first migration

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe(models.Model):
@@ -6,12 +11,18 @@ class Recipe(models.Model):
     description = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
-    preparation_time_unit = models.CharField()
+    preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
-    servings_unit = models.CharField()
+    servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
